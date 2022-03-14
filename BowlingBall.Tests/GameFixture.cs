b33aed22783bf168c35b7417d94f5872ae23a11f
surajkupare <99709243+surajkupare@users.noreply.github.com>
2022-03-14 07:19:45 +0530

    Initial commit

---
+++ BowlingBall.Tests/GameFixture.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingBall.Tests
+{
+	[TestClass]
+	public class GameFixture
+	{
+		public Game game;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			game = new Game();
+		}
+
+		/// <summary>
+		/// Test case to check when all pins are 0
+		/// </summary>
+		[TestMethod]
+		public void Gutter_game_score_should_be_zero_test()
+		{
+			Roll(game, 0, 20);
+			Assert.AreEqual(0, game.GetScore());
+		}
+
+		/// <summary>
+		/// Test case to check when all pins are 1 or 2
+		/// </summary>
+		[TestMethod]
+		[DataRow(1)]
+		[DataRow(2)]
+		public void Roll_all_with_same_pins_test(int pins)
+		{
+			Roll(game, pins, 20);
+			if (pins == 1)
+				Assert.AreEqual(20, game.GetScore());
+			else
+				Assert.AreEqual(40, game.GetScore());
+		}
+
+		/// <summary>
+		/// Test case to check when there is spare
+		/// </summary>
+		[TestMethod]
+		public void Roll_spare_test()
+		{
+			game.Roll(3);
+			game.Roll(3);
+			game.Roll(5);
+			game.Roll(5);
+			game.Roll(7);
+			Roll(game, 0, 15);
+			Assert.AreEqual(30, game.GetScore());
+		}
+
+		/// <summary>
+		/// Test case to check when there is strike
+		/// </summary>
+		[TestMethod]
+		public void Roll_strike_test()
+		{
+			game.Roll(10);
+			game.Roll(1);
+			game.Roll(2);
+			Roll(game, 0, 16);
+			Assert.AreEqual(16, game.GetScore());
+		}
+
+		/// <summary>
+		/// This method is used to set fixed pins to roll.
+		/// </summary>
+		/// <param name="game"></param>
+		/// <param name="pins"></param>
+		/// <param name="times"></param>
+		private void Roll(Game game, int pins, int times)
+		{
+			for (int i = 0; i < times; i++)
+			{
+				game.Roll(pins);
+			}
+		}
+	}
+}
---
+++ BowlingBall/Game.cs
@@ -0,0 +1,85 @@
+﻿namespace BowlingBall
+{
+    public class Game
+    {
+        /// <summary>
+        /// Rolls: Size of 21
+        /// </summary>
+        public int[] rolls = new int[21];
+
+        /// <summary>
+        /// CurretRole: intially set to 0
+        /// </summary>
+        public int currentRole = 0;
+
+        #region[PUBLIC METHODS]
+        /// <summary>
+        /// This will add pins to rolls.
+        /// </summary>
+        /// <param name="pins"></param>
+        public void Roll(int pins)
+        {
+            rolls[currentRole] = pins;
+            currentRole++;
+        }
+
+        /// <summary>
+        /// Returns the final score of the game.
+        /// </summary>
+        /// <returns></returns>
+        public int GetScore()
+        {
+            int score = 0;
+            int currentPinIndex = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsSpareInFrame(currentPinIndex)) 
+                {
+                    //if its spare then next 1 pin will be added into score as a bonus.
+                    score += rolls[currentPinIndex] + rolls[currentPinIndex + 1] + rolls[currentPinIndex + 2];
+                    currentPinIndex += 2;
+                }
+                else if (IsStrikeInFrame(currentPinIndex))
+                {
+                    //if its strike then next 2 pins will be added into score as a bonus.
+                    score += rolls[currentPinIndex] + rolls[currentPinIndex + 1] + rolls[currentPinIndex + 2];
+                    currentPinIndex++;
+                }
+                else
+                {
+                    //current pin + next pin.
+                    score += rolls[currentPinIndex] + rolls[currentPinIndex + 1];
+                    currentPinIndex += 2;
+                }
+            }
+            return score;
+        }
+        #endregion[PUBLIC METHODS]
+
+        #region[PRIVATE METHODS]
+
+       /// <summary>
+       /// This will check whether it is spare in a frame or not. 
+       /// e.g. 7,3 = 10. Hence it is spare.
+       /// </summary>
+       /// <param name="currentPinIndex"></param>
+       /// <returns></returns>
+        private bool IsSpareInFrame(int currentPinIndex)
+        {
+            return rolls[currentPinIndex] + rolls[currentPinIndex + 1] == 10 ? true: false;
+        }
+
+        /// <summary>
+        /// This will check whether it is strike in a frame or not. 
+        /// e.g. 10 in a first go itself. Hence it is strike.
+        /// </summary>
+        /// <param name="currentPinIndex"></param>
+        /// <returns></returns>
+        private bool IsStrikeInFrame(int currentPinIndex)
+        {
+            return rolls[currentPinIndex] == 10 ? true : false;
+        }
+        #endregion[PRIVATE METHODS]
+    }
+}
